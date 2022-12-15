624a511100ff89bcf89a494f966e08a54d44687e
Andrew Edwin McSwain <andrewmcswain@gmail.com>
2022-12-15 09:16:50 -0300

    first commit. not much to see yet

---
+++ compound_interest.rb
@@ -0,0 +1,16 @@
+
+
+  @principal = principal
+  @interest = interest
+  @period = period
+  @sub_period = sub_period
+  @installment = installment
+  @payout = payout
+
+@installment
+
+
+
+@yearly_payout = @sub_period
+
+@total_payout = @yearly_ @period 
